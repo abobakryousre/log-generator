6f18a0d325043ac11389743ca7668f4faaa467af
abobakr <abobakryousre@vodafone.com>
2023-03-16 22:40:52 +0200

    to 5 des-ip for top 10 deny username

--- stat-tool.py
+++ stat-tool.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-file_path = "traffic_logs__2023-03-16__21-21-00.json"
+file_path = "traffic_logs__2023-03-16__21-56-19.json"
 df = pd.read_json(file_path)
 
 def top_10_allow_source_ip(df):
@@ -8,3 +8,9 @@ def top_10_allow_source_ip(df):
 
 def to_10_deny_username(df):
     return df[df.action=="Deny"].groupby("username")['username'].count().sort_values(ascending=False).head(10)
+
+def top_5_des_ip_for_deny_username(df,users=to_10_deny_username(df)):
+    result = {}
+    for user in users.index:
+        result[user] = df[df.username==user].groupby("des-ip")['des-ip'].count().sort_values(ascending=False).head(5)
+    return result
